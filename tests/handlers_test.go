b638f29a5f5aab1445960187d761fba650cbaefe
rlebel12 <roblebel12@gmail.com>
2024-05-19 12:41:17 -0400

    callback handler test first pass complete

--- tests/handlers_test.go
+++ tests/handlers_test.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	"github.com/rlebel12/gosesh"
+	"github.com/rlebel12/gosesh/mocks"
 	"github.com/stretchr/testify/suite"
 	"golang.org/x/oauth2"
 )
@@ -106,6 +107,8 @@ func TestHandlersSuite(t *testing.T) {
 type Oauth2CallbackHandlerSuite struct {
 	suite.Suite
 	oauth2Server *httptest.Server
+	now          time.Time
+	withNow      func(*gosesh.Config)
 }
 
 func (s *Oauth2CallbackHandlerSuite) SetupSuite() {
@@ -115,11 +118,14 @@ func (s *Oauth2CallbackHandlerSuite) SetupSuite() {
 			http.Redirect(w, r, "http://localhost/auth/callback?code=code&state="+r.FormValue("state"), http.StatusTemporaryRedirect)
 		case "/token":
 			w.Header().Set("Content-Type", "application/json")
-			w.Write([]byte(`{"access_token":"access_token","token_type":"bearer","refresh_token":"refresh_token","expiry":"2021-01-01T00:00:00Z"}`))
+			_, err := w.Write([]byte(`{"access_token":"access_token","token_type":"bearer","refresh_token":"refresh_token","expiry":"2021-01-01T00:00:00Z"}`))
+			s.Require().NoError(err)
 		default:
 			http.Error(w, "not found", http.StatusNotFound)
 		}
 	}))
+	s.now = time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)
+	s.withNow = func(c *gosesh.Config) { c.Now = func() time.Time { return s.now } }
 }
 
 func (s *Oauth2CallbackHandlerSuite) TearDownSuite() {
@@ -133,12 +139,24 @@ func (s *Oauth2CallbackHandlerSuite) SetupTest() {
 type testCallbackRequestMode int
 
 const (
-	testCallbackErrNoStateCookie testCallbackRequestMode = iota
+	testCallbackSuccess testCallbackRequestMode = iota
+	testCallbackErrNoStateCookie
 	testCallbackInvalidStateCookie
 	testFailedExchange
+	testFailedUnmarshalRequest
+	testFailedUnmarshalReadBody
+	testFailedUnmarshalDataFinal
+	testCallbackErrUpsertUser
+	testCallbackErrCreateSession
 )
 
-func (s *Oauth2CallbackHandlerSuite) makeInputs(mode testCallbackRequestMode) (r *http.Request, config *oauth2.Config) {
+type failReader struct{}
+
+func (m *failReader) Read(p []byte) (n int, err error) {
+	return 0, fmt.Errorf("failed read")
+}
+
+func (s *Oauth2CallbackHandlerSuite) makeInputs(mode testCallbackRequestMode) (r *http.Request, config *oauth2.Config, user *mocks.OAuth2User, store *mocks.Storer) {
 	var err error
 	callbackURL := fmt.Sprintf("%s/auth/callback", s.oauth2Server.URL)
 	r, err = http.NewRequest(http.MethodGet, callbackURL, nil)
@@ -176,32 +194,160 @@ func (s *Oauth2CallbackHandlerSuite) makeInputs(mode testCallbackRequestMode) (r
 		return
 	}
 	config.Endpoint.TokenURL = fmt.Sprintf("%s/token", s.oauth2Server.URL)
+	user = mocks.NewOAuth2User(s.T())
+
+	if mode == testFailedUnmarshalRequest {
+		user.EXPECT().Request(r.Context(), "access_token").Return(nil, fmt.Errorf("failed request"))
+		return
+	}
+
+	if mode == testFailedUnmarshalReadBody {
+		response := &http.Response{
+			Body: io.NopCloser(&failReader{}),
+		}
+		user.EXPECT().Request(r.Context(), "access_token").Return(response, nil)
+		return
+	}
+	response := &http.Response{
+		Body: io.NopCloser(strings.NewReader("")),
+	}
+	user.EXPECT().Request(r.Context(), "access_token").Return(response, nil)
+
+	if mode == testFailedUnmarshalDataFinal {
+		user.EXPECT().Unmarshal([]byte{}).Return(fmt.Errorf("failed unmarshal"))
+		return
+	}
+	user.EXPECT().Unmarshal([]byte{}).Return(nil)
+	user.EXPECT().String().Return("user")
+	store = mocks.NewStorer(s.T())
+
+	if mode == testCallbackErrUpsertUser {
+		store.EXPECT().UpsertUser(r.Context(), user).Return(nil, fmt.Errorf("failed upsert"))
+		return
+	}
+	identifier := mocks.NewIdentifier(s.T())
+	identifier.EXPECT().String().Return("identifier")
+	store.EXPECT().UpsertUser(r.Context(), user).Return(identifier, nil)
+	createSessionRequest := gosesh.CreateSessionRequest{
+		UserID:   identifier,
+		IdleAt:   s.now.Add(1 * time.Hour),
+		ExpireAt: s.now.Add(24 * time.Hour),
+	}
+
+	if mode == testCallbackErrCreateSession {
+		store.EXPECT().CreateSession(r.Context(), createSessionRequest).
+			Return(nil, fmt.Errorf("failed create session"))
+		return
+	}
+	store.EXPECT().CreateSession(r.Context(), createSessionRequest).Return(&gosesh.Session{
+		ID:       identifier,
+		UserID:   identifier,
+		IdleAt:   s.now.Add(1 * time.Hour),
+		ExpireAt: s.now.Add(24 * time.Hour),
+	}, nil)
 
 	return
 }
 
 func (s *Oauth2CallbackHandlerSuite) TestErrNoStateCookie() {
 	rr := httptest.NewRecorder()
-	sesh := gosesh.New(nil, nil)
-	request, config := s.makeInputs(testCallbackErrNoStateCookie)
+	sesh := gosesh.New(nil, nil, s.withNow)
+	request, config, _, _ := s.makeInputs(testCallbackErrNoStateCookie)
 	err := sesh.OAuth2Callback(rr, request, nil, config)
 	s.EqualError(err, "failed getting state cookie: http: named cookie not present")
 }
 
 func (s *Oauth2CallbackHandlerSuite) TestErrInvalidStateCookie() {
 	rr := httptest.NewRecorder()
-	sesh := gosesh.New(nil, nil)
-	request, config := s.makeInputs(testCallbackInvalidStateCookie)
+	sesh := gosesh.New(nil, nil, s.withNow)
+	request, config, _, _ := s.makeInputs(testCallbackInvalidStateCookie)
 	err := sesh.OAuth2Callback(rr, request, nil, config)
+	s.assertStateCookieReset(rr.Result())
 	s.EqualError(err, "invalid state cookie")
 }
 
 func (s *Oauth2CallbackHandlerSuite) TestFailedExchange() {
 	rr := httptest.NewRecorder()
-	sesh := gosesh.New(nil, nil)
-	request, config := s.makeInputs(testFailedExchange)
+	sesh := gosesh.New(nil, nil, s.withNow)
+	request, config, _, _ := s.makeInputs(testFailedExchange)
 	err := sesh.OAuth2Callback(rr, request, nil, config)
+	s.assertStateCookieReset(rr.Result())
 	s.EqualError(err, "failed exchanging token: oauth2: cannot fetch token: 404 Not Found\nResponse: not found\n")
+}
+
+func (s *Oauth2CallbackHandlerSuite) TestFailUnmarshalUserDataRequest() {
+	rr := httptest.NewRecorder()
+	sesh := gosesh.New(nil, nil, s.withNow)
+	request, config, user, _ := s.makeInputs(testFailedUnmarshalRequest)
+	err := sesh.OAuth2Callback(rr, request, user, config)
+	s.assertStateCookieReset(rr.Result())
+	s.EqualError(err, "failed unmarshalling data: failed getting user info: failed request")
+}
+
+func (s *Oauth2CallbackHandlerSuite) TestFailUnmarshalUserDataReadBody() {
+	rr := httptest.NewRecorder()
+	sesh := gosesh.New(nil, nil, s.withNow)
+	request, config, user, _ := s.makeInputs(testFailedUnmarshalReadBody)
+	err := sesh.OAuth2Callback(rr, request, user, config)
+	s.assertStateCookieReset(rr.Result())
+	s.EqualError(err, "failed unmarshalling data: failed read response: failed read")
+}
+
+func (s *Oauth2CallbackHandlerSuite) TestFailUnmarshalDataFinal() {
+	rr := httptest.NewRecorder()
+	sesh := gosesh.New(nil, nil, s.withNow)
+	request, config, user, _ := s.makeInputs(testFailedUnmarshalDataFinal)
+	err := sesh.OAuth2Callback(rr, request, user, config)
+	s.assertStateCookieReset(rr.Result())
+	s.EqualError(err, "failed unmarshalling data: failed unmarshal")
+}
+
+func (s *Oauth2CallbackHandlerSuite) TestCallbackErrUpsertUser() {
+	request, config, user, store := s.makeInputs(testCallbackErrUpsertUser)
+	rr := httptest.NewRecorder()
+	sesh := gosesh.New(nil, store, s.withNow)
+	err := sesh.OAuth2Callback(rr, request, user, config)
+	s.assertStateCookieReset(rr.Result())
+	s.EqualError(err, "failed upserting user: failed upsert")
+}
+
+func (s *Oauth2CallbackHandlerSuite) TestCallbackErrCreateSession() {
+	request, config, user, store := s.makeInputs(testCallbackErrCreateSession)
+	rr := httptest.NewRecorder()
+	sesh := gosesh.New(nil, store, s.withNow)
+	err := sesh.OAuth2Callback(rr, request, user, config)
+	s.assertStateCookieReset(rr.Result())
+	s.EqualError(err, "failed creating session: failed create session")
+}
+
+func (s *Oauth2CallbackHandlerSuite) TestCallbackSuccess() {
+	request, config, user, store := s.makeInputs(testCallbackSuccess)
+	rr := httptest.NewRecorder()
+	sesh := gosesh.New(nil, store, s.withNow)
+	err := sesh.OAuth2Callback(rr, request, user, config)
+	s.Require().NoError(err)
+	response := rr.Result()
+	s.Equal(2, len(response.Cookies()))
+	sessionCookie := response.Cookies()[1]
+	s.Equal("session", sessionCookie.Name)
+	s.Equal("aWRlbnRpZmllcg==", sessionCookie.Value)
+	s.Equal(s.now.Add(24*time.Hour), sessionCookie.Expires)
+	s.Equal("localhost", sessionCookie.Domain)
+	s.Equal("/", sessionCookie.Path)
+	s.Equal(http.SameSiteLaxMode, sessionCookie.SameSite)
+	s.False(sessionCookie.Secure)
+	s.assertStateCookieReset(response)
+}
+
+func (s *Oauth2CallbackHandlerSuite) assertStateCookieReset(response *http.Response) {
+	cookie := response.Cookies()[0]
+	s.Equal("oauthstate", cookie.Name)
+	s.Equal("", cookie.Value)
+	s.Equal(s.now, cookie.Expires)
+	s.Equal("localhost", cookie.Domain)
+	s.Equal("/", cookie.Path)
+	s.Equal(http.SameSiteLaxMode, cookie.SameSite)
+	s.False(cookie.Secure)
 }
 
 func TestOauth2CallbackHandlerSuite(t *testing.T) {
