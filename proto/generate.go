6bd53a0cea05ac53415789d7b02f6e2964bbe3da
rlebel12 <roblebel12@gmail.com>
2024-06-11 19:50:18 -0400

    add pnpm for ts-proto

--- proto/generate.go
+++ proto/generate.go
@@ -1,3 +1,4 @@
 package proto
 
 //go:generate protoc -I=. --go_out=. --go-grpc_out=. --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative gosesh.proto
+//go:generate protoc -I=. --ts_proto_opt=paths=source_relative --ts_proto_opt=oneof=unions --ts_proto_opt=useNullAsOptional=true --ts_proto_opt=initializeFieldsAsUndefined=false --plugin=../node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=. gosesh.proto
